17be438be7a2d855e39c6a45cdd807244f7def65
MaxEdgington <edgington.m.w@gmail.com>
2023-08-07 14:58:56 -0400

    added a new route for /hello that sends back Hello World bolded within HTML.

--- express_server.js
+++ express_server.js
@@ -15,6 +15,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// adding route
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+//adding route
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
