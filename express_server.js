2923f6c62c9eb0f3d2f11886bcb4d8453a5e1319
MaxEdgington <edgington.m.w@gmail.com>
2023-08-07 16:27:16 -0400

    sending data to urls_index.ejs template from express

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// installed ejs with npm install ejs
+// set ejs as view engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -23,4 +27,9 @@ app.get("/urls.json", (req, res) => {
 //adding route
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars); // passing url data to our template
 });
